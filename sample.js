9ea0638cc08ae0dd413e8ecdf0f9231802f2303d
k-isaka <isk.rugby@gmail.com>
2023-04-19 22:26:02 +0900

    feat: first commit

---
+++ sample.js
@@ -0,0 +1,5 @@
+// 参考: https://www.tohoho-web.com/ex/typescript.html
+function hello(name) {
+    console.log("hello " + name);
+}
+hello("Kou");
---
+++ sample.ts
@@ -0,0 +1,7 @@
+// 参考: https://www.tohoho-web.com/ex/typescript.html
+
+function hello(name: string): void {
+    console.log("hello " + name);
+}
+
+hello("Kou");
