150cf73cb6142b7d6845e46930a2dca5bc683c8a
k-isaka <isk.rugby@gmail.com>
2023-04-20 19:42:08 +0900

    feat: regenerated project

--- sample.js
+++
@@ -1,5 +0,0 @@
-// 参考: https://www.tohoho-web.com/ex/typescript.html
-function hello(name) {
-    console.log("hello " + name);
-}
-hello("Kou");
--- sample.ts
+++
@@ -1,7 +0,0 @@
-// 参考: https://www.tohoho-web.com/ex/typescript.html
-
-function hello(name: string): void {
-    console.log("hello " + name);
-}
-
-hello("Kou");
