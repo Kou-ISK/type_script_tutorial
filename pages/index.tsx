507176c18a8e8acce8ffe1aae21c308590e14e69
k-isaka <isk.rugby@gmail.com>
2023-04-22 09:14:46 +0900

    feat: ボタンのスタイルを変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,17 @@ import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 
 // getServerSidePropsから渡されるpropsの型
-type Props ={
-    initialImageUrl:string;
+type Props = {
+    initialImageUrl: string;
 };
 
-const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
     // useStateで状態を定義
     const [imageUrl, setImageUrl] = useState(initialImageUrl); //初期値を渡す
     const [loading, setLoading] = useState(false);
 
     // ボタンクリックで画像を読み込む
-    const handleClick = async ()=>{
+    const handleClick = async () => {
         setLoading(true);
         const newImage = await fetchImage();
         setImageUrl(newImage.url);
@@ -24,15 +24,15 @@ const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
 
     // IndexPageの戻り値？
     return (
-    <div className={styles.page}>
-        <button onClick={handleClick} className={styles.button}>他の猫も見る</button>
-        <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img}/>}</div>
-    </div>);
+        <div className={styles.page}>
+            <button onClick={handleClick} style={{ backgroundColor: "#319795", border: "none", borderRadius: "4px", color: "white", padding: "4px 8px", }}>別のにゃんこも見る</button>
+            <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img} />}</div>
+        </div>);
 };
 export default IndexPage;
 
 // サーバーサイドで実行する処理
-export const getServerSideProps: GetServerSideProps<Props> = async()=>{
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
     const image = await fetchImage();
     return {
         props: {
